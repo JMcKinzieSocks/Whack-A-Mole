c3434ce0e655fe5f3fd568253a4c7baeb32f810f
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2020-10-06 13:46:44 -0500

    Added button event

--- Whack-A-Mole/ViewController.swift
+++ Whack-A-Mole/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("It worked!")
+    }
+    
 }
 
