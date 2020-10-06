4ffb7f798029866f6a4e285cfc883dcfb21ba9fc
Jackson McKinzie <jacksondmckinzie@gmail.com>
2020-10-06 13:22:59 -0500

    Initial Commit

---
+++ Whack-A-Mole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whack-A-Mole
+//
+//  Created by McKinzie, Jackson D on 10/6/20.
+//  Copyright © 2020 McKinzie, Jackson D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
