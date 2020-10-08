9e221df13e9d5f02d65e5e77d9fbf64546ff0d01
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2020-10-08 13:16:41 -0500

    Added random movement to button

--- Whack-A-Mole/ViewController.swift
+++ Whack-A-Mole/ViewController.swift
@@ -12,17 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var button: UIButton!
-    
+    var score: Int = 0
+    let screenSize = UIScreen.main.bounds
+    var screenWidth = 0
+    var screenHeight = 0
+    let diameter = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        screenWidth = Int(screenSize.width)
+        screenHeight = Int(screenSize.height)
+        button.frame = CGRect(x: 150,y: 200, width: diameter, height: diameter)
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
-        scoreLabel.text = String((Int(scoreLabel.text ?? "0") ?? 0) + 1)
+        score += 1
+        scoreLabel.text = String(score)
         print("It worked!")
-        button.isHidden = true
+        //button.isHidden = true
+        let maxXRight = (screenWidth - 20) - diameter
+        let maxYBottom = (screenHeight - 20) - diameter
+        let randomX = Int.random(in: 20...maxXRight)
+        let randomY = Int.random(in: 20...maxYBottom) + 56
+        
+        button.frame = CGRect(x: randomX, y: randomY, width: diameter, height: diameter)
         
     }
     
