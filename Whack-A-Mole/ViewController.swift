e24c657b2da9aede96b8aba6a38778ef2e4f9827
Jackson McKinzie <jackson.mckinzie.k12.friscoisd.org>
2020-10-13 14:11:30 -0500

    Added ending conditions and refactored code

--- Whack-A-Mole/ViewController.swift
+++ Whack-A-Mole/ViewController.swift
@@ -17,51 +17,78 @@ class ViewController: UIViewController {
     let screenSize: CGRect = UIScreen.main.bounds
     var screenWidth: Int = 0
     var screenHeight: Int = 0
+    let margin: Int = 20
     var diameter: Int = 50
-    var buttonHit: Bool = false
     var score: Int = 0
     var time: Int = 5
     var timer = Timer()
+    var wasHit: Bool = false
+    var totalMoles: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initialize()
-        view.addSubview(firstButton)
-        self.view = view
     }
 
     func initialize() {
+        //Capturing screen dimensions
         screenWidth = Int(screenSize.width)
         screenHeight = Int(screenSize.height)
+        
+        //Background setup
+        background.frame = CGRect(x: margin,y: margin * 3, width: screenWidth - (margin * 2), height: screenHeight - margin * 4)
+        background.backgroundColor = UIColor.green
+        view.addSubview(background)
+        
+        //Score label setup
+        scoreLabel.frame = CGRect(x: margin, y: margin * 2, width: screenWidth - (margin * 2), height: margin)
+        scoreLabel.text = String(score)
+        view.addSubview(scoreLabel)
+        
+        //Initial mole and timer configuration
         timer = Timer.scheduledTimer(timeInterval: Double(time), target: self, selector: #selector(updateButton), userInfo: nil, repeats: true)
-        firstButton.frame = CGRect(x: screenWidth / 2,y: screenHeight / 3, width: diameter, height: diameter)
+        firstButton.frame = getRandomPosition()
         firstButton.backgroundColor = UIColor.brown
         firstButton.addTarget(self, action:#selector(buttonPressed(_:)), for: .touchUpInside)
+        view.addSubview(firstButton)
+        self.view = view
     }
     
+    //If the button is pressed, do this
     @objc func buttonPressed(_ sender: UIButton!) {
-        buttonHit = true
-        score += 1
-        scoreLabel.text = String(score)
         timer.invalidate()
+        wasHit = true
         updateButton()
         timer = Timer.scheduledTimer(timeInterval: Double(time), target: self, selector: #selector(updateButton), userInfo: nil, repeats: true)
     }
     
+    //Updates score and mole location
     @objc func updateButton() {
-        print("It worked!")
-        firstButton.isHidden = true
+        totalMoles += 1
+        score = wasHit ? score + 1 : score - 1
+        scoreLabel.text = String(score)
+        if totalMoles <= 10 { //if less than 10 moles have been in play, continue
+            wasHit = false
+            firstButton.isHidden = true
+            firstButton.frame = getRandomPosition()
+            firstButton.isHidden = false
+            view.addSubview(firstButton)
+            self.view = view
+        } else { //after 10th mole, end game
+            timer.invalidate()
+            firstButton.isHidden = true
+            scoreLabel.text = "Final score: " + String(score)
+        }
+    }
+    
+    //Generates CGRect for mole to appear at
+    func getRandomPosition() -> CGRect {
         let maxXRight: Int = (screenWidth - 20) - diameter
         let maxYBottom: Int = (screenHeight - 20) - diameter
         let randomX: Int = Int.random(in: 20...maxXRight)
         let randomY: Int = Int.random(in: 20...maxYBottom - 56) + 56
-        firstButton.frame = CGRect(x: randomX, y: randomY, width: diameter, height: diameter)
-        buttonHit = false
-        firstButton.isHidden = false
-        view.addSubview(firstButton)
-        self.view = view
+        return CGRect(x: randomX, y: randomY, width: diameter, height: diameter)
     }
-    
     
 }
 
